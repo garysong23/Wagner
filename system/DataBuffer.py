28b779d992ccb1150bb41cf5d9e6bcd263344766
Gary Song <garysong23@gmail.com>
2017-11-27 22:23:46 -0500

    clean up threads

--- system/DataBuffer.py
+++ system/DataBuffer.py
@@ -1,6 +1,25 @@
+import threading
+import time
+import sys
+
 class DataBuffer:
   def __init__(self):
     self.val = 0
+    self._bg_thread = self._setup_bg_thread()
 
-  def on_input(self, val): self.val = val
-  def status(self): return self.val
+  def on_input(self, val):
+    if (val == 'stop') or (val == 'exit'):
+      print("Terminating: I'll be back")
+      sys.exit()
+    self.val = val
+
+  def _setup_bg_thread(self):
+    thread = threading.Thread(target=self._background)
+    thread.daemon = True
+    thread.start()
+    return thread
+
+  def _background(self):
+    while True:
+      time.sleep(5)
+      print('Current val: ', self.val)
--- threads.py
+++ threads.py
@@ -1,16 +1,8 @@
-import threading
-import time
-import sys
-
 from system.DataBuffer import DataBuffer
-from system.Threads import Threads
 from system.DataStream import DataStream
 
 data_stream = DataStream()
 data_buffer = DataBuffer()
-threads = Threads(data_buffer)
-
-data_stream.subscribe(threads.on_input)
 data_stream.subscribe(data_buffer.on_input)
 
 while True:
