cda1e4864178db0a22b8213fdeda4b6c2a0e7490
Gary Song <garysong23@gmail.com>
2017-12-01 13:54:10 -0500

    complete song transition

--- system/SongPicker.py
+++ system/SongPicker.py
@@ -2,12 +2,16 @@ import json
 import random
 import librosa
 import numpy as np
+
 from audio.Song import Song
+from audio.Transition import Transition
+
 from system.signal_action_constants import SIG_ACTIONS
 
 SR = 44100
+MIX_LEN = 32
 
-name = 'three_songs'
+name = 'full'
 DATA_SET = './data/json/' + name + '.json'
 
 class SongPicker:
@@ -18,14 +22,15 @@ class SongPicker:
   def pick_song(self, sig_action):
     if not self._cur_song:
       # TODO: choose inital song from prexisting signals
-      # TODO: setup first song intro process
       chosen_song = self._all_songs[128][0]
+      raw_audio = self._retrieve_first_song_raw_audio(chosen_song)
     else:
       chosen_song = self._find_song_for_action(sig_action)
-    self._cur_song = chosen_song
+      raw_audio = self._retrieve_raw_audio(chosen_song)
 
+    self._cur_song = chosen_song
     print('[SongPicker] - Picked:', chosen_song)
-    return self._retrieve_raw_audio(chosen_song)
+    return raw_audio
 
   def _load_songs(self):
     print('[SongPicker] - Loading Songs')
@@ -64,6 +69,16 @@ class SongPicker:
     song = random.choice(self._all_songs[new_bpm])
     return song
 
+  # TODO: setup intro to song
+  def _retrieve_first_song_raw_audio(self, song):
+    trans_in = song.trans_in_audio()
+    body = song.body_audio()
+    raw_audio = np.concatenate([trans_in, body], axis=1)
+    return raw_audio
+
   def _retrieve_raw_audio(self, song):
-    # TODO: stitch songs
-    return song.body_audio()
+    out_song, in_song = self._cur_song, song
+    transition = Transition(out_song, in_song, MIX_LEN)
+    trans_audio = transition.merged_audio
+    raw_audio = np.concatenate([trans_audio, in_song.body_audio()], axis=1)
+    return raw_audio
